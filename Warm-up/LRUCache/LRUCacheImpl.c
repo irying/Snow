8c2ef6f9f413eddbb11ba88a1ce68148344063e0
irying <kyrieup@gmail.com>
2017-06-06 23:42:35 +0800

    LRU--list

--- Warm-up/LRUCache/LRUCacheImpl.c
+++ Warm-up/LRUCache/LRUCacheImpl.c
@@ -44,12 +44,81 @@ static void freeCacheEntry(cacheEntry* entry)
     free(entry);
 }
 
-static cacheEntry* insert(LRUCache *cache, cacheEntry *entry)
+static void removeFromList(LRUCache *cache, cacheEntry *entry)
+{
+    if (cache->lruListSize <= 0)
+        return;
+
+    // 4种情况，这个列表只剩一个元素
+    // 这个元素是头
+    // 这个元素是尾
+    // 这个元素在中间
+
+    if (entry == cache->lruListHead && entry == cache->lruListTail) {
+        cache->lruListHead = cache->lruListTail = NULL;
+    } else if (entry == cache->lruListHead) {
+        cache->lruListHead = entry->lruListNext;
+        cache->lruListHead->lruListPrev = NULL;
+    } else if(entry == cache->lruListTail) {
+        cache->lruListTail = entry->lruListPrev;
+        cache->lruListTail->lruListNext = NULL;
+    } else {
+        entry->lruListPrev->lruListNext = entry->lruListNext;
+        entry->lruListNext->lruListPrev = entry->lruListPrev;
+    }
+
+    cache->lruListSize--;
+}
+
+static cacheEntry* insertToListHead(LRUCache *cache, cacheEntry *entry)
 {
     cacheEntry *removeEntry = NULL;
 
-    if (++cache->lruListSize > cache->ca)
+    // 缓存满了，需要删除链表表尾节点，淘汰最久没有被访问的缓存单元
+    // 链表为空
+    // 链表非空
+    if (++cache->lruListSize > cache->capacity) {
+        removeEntry = cache->lruListTail;
+        removeFromList(cache, removeEntry);
+    }
+
+    if (cache->lruListHead == NULL && cache->lruListTail == NULL) {
+        cache->lruListHead = cache->lruListTail = entry;
+    } else {
+        entry->lruListPrev = NULL;
+        entry->lruListNext = cache->lruListHead;
+        cache->lruListHead->lruListPrev = entry;
+        cache->lruListHead = entry;
+    }
+
+    return removeEntry;
 }
+
+static void freeList(LRUCache *cache)
+{
+    // 链表为空
+    // 链表不为空
+    if (0 == cache->lruListSize) return;
+
+    cacheEntry *entry = cache->lruListHead;
+
+    while(entry) {
+        cacheEntry *temp = entry->lruListNext;
+        freeCacheEntry(entry);
+        entry = temp;
+    }
+    cache->lruListSize = 0;
+}
+
+static void updateLRUList(LRUCache *cache, cacheEntry *entry)
+{
+    removeFromList(cache, entry);
+    insertToListHead(cache, entry);
+}
+
+
+
+
 
 
 
